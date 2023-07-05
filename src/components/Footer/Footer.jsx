4e4514dce0b5b7d54e705405871908dada4a221d
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-07-05 19:30:46 +0300

    update
    add symbol

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { FooTer, Text } from './Footer.styled';
 const Footer = () => {
   return (
     <FooTer>
-      <Text>Copyright © 2023. Created by Vladyslav Kutsevych.</Text>
+      <Text>Copyright &copy; 2023. Created by Vladyslav Kutsevych.</Text>
     </FooTer>
   );
 };
